b4dc2b6d2092065b1373db885e0022a85d9f444a
DanArmstrong124 <danielarmstrong124@gmail.com>
2020-06-23 13:11:30 +0000

    Fixed port

--- app.py
+++ app.py
@@ -2,18 +2,16 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-from os import path
-if path.exists("env.py"):
+if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
 
-#Link the database into PyMongo
-mongo_db_name = os.environ.get('MNGDB_NAME')
-mongo_db_pass = os.environ.get('MNGDB_PASS')
+# Link the database into PyMongo
 
-app.config["MONGO_DBNAME"] = mongo_db_name
-app.config["MONGO_URI"] = mongo_db_pass
+app.config["MONGO_DBNAME"] = "cook_book"
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
 
 mongo = PyMongo(app)
 
@@ -23,7 +21,8 @@ mongo = PyMongo(app)
 def get_meals():
     return render_template("meals.html", meals=mongo.db.meals.find())
 
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-        port=int(os.environ.get('PORT')),
-        debug=True)
+            port=int(os.environ.get('PORT')),
+            debug=True)
