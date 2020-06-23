29939949619e1ca27471b9b0af322ac328c99a8b
DanArmstrong124 <danielarmstrong124@gmail.com>
2020-06-23 15:07:24 +0000

    Added Form

--- app.py
+++ app.py
@@ -15,11 +15,22 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
 mongo = PyMongo(app)
 
-
 @app.route('/')
 @app.route('/get_meals')
 def get_meals():
     return render_template("meals.html", meals=mongo.db.meals.find())
+
+@app.route('/add_meals')
+def add_meals():
+    return render_template("addmeal.html",
+    categories=mongo.db.categories.find())
+
+@app.route('/insert_meal', methods=['POST'])
+def insert_meal():
+    meals = mongo.db.meals
+    meals.insert_one(request.form)
+
+
 
 
 if __name__ == '__main__':
